73c1ebfe1b1230a6f7fba4d3dea25a7037f341c7
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2018-09-20 15:54:54 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,7 +26,7 @@ client.on('message', async msg => {
   if(command === 'purge' || command === 'clean' || command === 'clear') {
     const deleteCount = parseInt(args[0], 10);
     
-    if(!deleteCount || deleteCount < 1 || deleteCount > 100)
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
       return msg.reply("Please provide a number between 1 and 100 for the number of messages to delete!");
     
     const fetched = await msg.channel.fetchMessages({limit: deleteCount});
